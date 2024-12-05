13059ec89ea4cb1a38a2fa6ff6baa0d22380e871
RifatExplorer <rifat.official.13@gmail.com>
2024-12-05 20:10:35 +0600

    improve information

--- abc.py
+++ abc.py
@@ -1,1 +1,6 @@
-print("Hello github!")
+# This programm add two number
+a =int(input("Enter a number: "))
+b =int(input("Enter another number: "))
+
+c = a + b
+print(f"The sum of {a} and {b} is:{c}. ")
